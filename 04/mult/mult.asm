3c9454bc21026d7d1f216b88539061f6595bcdd7
AndrewDTN <datoo_andrew@yahoo.com.tw>
2019-12-13 19:39:53 +0800

    ch4mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+    //int R0=3;
+    //@0
+    //M=3
+    //int R1=5;
+    //@1
+    //M=5
+    //int R2=0;  
+    @2
+    M=0   
+
+(LOOP) // LOOP:
+    // if(R0<=0) goto END;// while(R0>0) 
+    @0
+    D=M
+    @END
+    D;JLE 
+    // R2=R1+R2;
+    @1
+    D=M
+    @2
+    M=D+M
+    //R0-=1;
+    @0
+    M=M-1
+    //goto LOOP; 
+    @0
+    D=M
+    @LOOP
+    D;JGT
+(END)//END:
+   @END
+   0;JMP
--- 04/mult/multgoto.c
+++ 04/mult/multgoto.c
@@ -6,11 +6,14 @@ int main()
     int R1=5;
     int R2=0; // @2, M=0
 
-    while(R0>0)
-    {
+LOOP:
+    if(R0<=0) goto END;// while(R0>0)
+    // {
         R2=R1+R2;
         R0-=1;
         printf("R0=%d,R1%d,R2%d\n",R0,R1,R2);
-    }
+    // }
+    goto LOOP; 
+END:
     printf("R2=%d\n",R2);
 }
